f9b8b4dbc56e6784b62d9e9e40c81b471d91dd52
hh <hh@hhmbp15.local>
2020-06-29 19:09:59 +0100

    moving_zeroes.py complete

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -5,7 +5,25 @@ Returns: a List of integers
 def moving_zeroes(arr):
     # Your code here
 
-    pass
+
+	# this solution is a single pass through the input array
+	# with O(1) space
+    for i in range(len(arr)):
+        if arr[i] == 0:
+            x = arr[i]
+            arr.remove(x)
+            arr.append(x)
+    return arr
+
+    # this is an alternative solution, single pass through
+    # the input array in O(1) space because as n increases
+    # we still only store one value in 'count'
+
+	# count = arr.count(0)
+	# for i in range(0, count):
+	# 	arr.remove(0)
+	# 	arr.append(0)
+	# return arr
 
 
 if __name__ == '__main__':
